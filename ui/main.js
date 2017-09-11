2c7a41e900d6e3ed002337ff21d755a3c3dbe70c
bmk15897 <31247303+bmk15897@users.noreply.github.com>
2017-09-11 19:52:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = 'New Value';
 var img=document.getElementById('img');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginleft+10;
+    marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
