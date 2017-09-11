15eea335a8470f7262829f5f733ad6d6c487ae15
bmk15897 <31247303+bmk15897@users.noreply.github.com>
2017-09-11 19:36:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var im=document.getElementById('madi');
+element.onclick().function(){
+    img.style.marginLeft('100px');
+};
